daafa87ccf510434a7ebf3fddcc5dd740f08ce9a
Evgenii <osipenkov.evgenii@gmail.com>
2023-11-29 17:05:50 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Bug with useCookie on SSR</h1>
+
+    <button @click="setCookie">Set cookie to 'foo'</button>
+
+    <p>Cookie value: {{ `${foo}` }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const foo = useCookie<string | undefined>('foo')
+
+const setCookie = () => (foo.value = 'foo')
+</script>
